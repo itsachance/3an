52f0fddbc8e9b000a1bec4161a296266806162a8
ItsAchance <contact@alexanderchance.me>
2026-03-23 21:21:45 +0100

    I don't know man, it's a lot here

--- go.mod
+++ go.mod
@@ -12,8 +12,10 @@ require (
 	github.com/cli/browser v1.3.0 // indirect
 	github.com/fatih/color v1.16.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-sqlite3 v1.14.37 // indirect
 	github.com/natefinch/atomic v1.0.1 // indirect
 	golang.org/x/mod v0.26.0 // indirect
 	golang.org/x/net v0.42.0 // indirect
--- main.go
+++ main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
-	"github.com/ItsAchance/3an/handlers"
+
+	_"github.com/ItsAchance/3an/handlers"
+	"github.com/ItsAchance/3an/internal/models"
 )
 
 func main() {
+	models.Db()
 	mux := http.NewServeMux()
 	// fileServer := http.FileServer(http.Dir(".ui/html"))
 	// mux.Handle("GET /html/", http.StripPrefix("/html", fileServer))
 
-	mux.HandleFunc("GET /{$}", handlers.Home)
-	mux.HandleFunc("GET /get-score", handlers.Highscore)
+	// mux.HandleFunc("GET /{$}", handlers.Home)
+	// mux.HandleFunc("GET /get-score",handlers.Highscore )
 
 	log.Printf("%s", "Listening on http://localhost:5500")
 	err := http.ListenAndServe(":5500", mux)
