e3c201c389c9c70cd7967ceb2f90044f1c53b318
ItsAchance <contact@alexanderchance.me>
2026-06-14 21:19:25 +0200

    chore(refactor): Lots of refactoring
     - Added routes to routes.go from main.go
     - SaveGame handler is under development, is new from old JS app
     - Snippets got cleaned up to just hold query methods

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,14 +1,29 @@
-package handlers
+package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 	"text/template"
+	"time"
 
 	"github.com/ItsAchance/3an/internal/models"
 )
 
 type Application struct {
-	SnippetModel *models.SnippetModel
+	ScoreModel *models.ScoreModel
+}
+
+type Data struct {
+	ID      int       `json:"id,omitempty"`
+	Name    string    `json:"name,omitempty"`
+	Score   int       `json:"score,omitempty"`
+	Created time.Time `json:"created,omitempty"`
+}
+
+type GameStats struct {
+	GameStats map[string]Data `json:"gameStats,omitempty"`
 }
 
 func (app *Application) Home(w http.ResponseWriter, r *http.Request) {
@@ -17,6 +32,34 @@ func (app *Application) Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *Application) Highscore(w http.ResponseWriter, r *http.Request) {
-	tmlp := template.Must(template.ParseFiles("./ui/html/index.html"))
-	tmlp.Execute(w, nil)
+	name, score := app.ScoreModel.GetHighscore()
+
+	data := &Data{
+		Name:  name,
+		Score: score,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+}
+
+func (app *Application) SaveGame(w http.ResponseWriter, r *http.Request) {
+	saveScore := &GameStats{}
+	respByte, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Errorf("Error: %w", err)
+	}
+	err = json.Unmarshal(respByte, &saveScore)
+	if err != nil {
+		fmt.Errorf("Error unmarshaling: %w", err)
+	}
+	for _, playerStats := range saveScore.GameStats {
+		fmt.Printf("Player: %s, Score: %d\n", playerStats.Name, playerStats.Score)
+	}
+	// populate a models.Score object from above codee and pass it in the app.ScoreModel.SaveScore method
+
+	// err = app.ScoreModel.SaveScore(saveScore)
+	//
+	//	if err != nil {
+	//		fmt.Errorf("error when saving score: %w", err)
+	//	}
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,25 +1,32 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 
-	"github.com/ItsAchance/3an/handlers"
-	"github.com/ItsAchance/3an/internal/models"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	app := &handlers.Application{}
-	models.CreateDb()
-	mux := http.NewServeMux()
+	db, err := sql.Open("sqlite3", "file:trean.db?_journal_mode=WAL&_synchronous=NORMAL")
+	if err != nil {
+		log.Printf("An error has occured: %s\n", err)
+	}
+	defer db.Close()
 
-	 fileServer := http.FileServer(http.Dir("./ui/static"))
-	 mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	query := `CREATE TABLE IF NOT EXISTS scoreboard(
+		id INTEGER PRIMARY KEY,
+		name TEXT NOT NULL UNIQUE,
+		score INTEGER NOT NULL,
+		created DATETIME DEFAULT CURRENT_TIMESTAMP);`
 
-	 mux.HandleFunc("GET /{$}", app.Home)
-	 mux.HandleFunc("GET /get-score",app.Highscore)
+	_, err = db.Exec(query)
+	if err != nil {
+		log.Printf("SQL table couldn't be created: %v", err)
+	}
 
 	log.Printf("%s", "Listening on http://localhost:5500")
-	err := http.ListenAndServe(":5500", mux)
+	err = http.ListenAndServe(":5500", Routes(db))
 	log.Fatal(err)
 }
